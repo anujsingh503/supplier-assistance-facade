1313b8ff5deaa905f0166024323e89e28f598952
Anuj Singh <anujs503@gmail.com>
2021-08-19 00:31:48 +0100

    Project initialized

---
+++ src/main/java/com/supplier/SupplierAssistanceFacadeApplication.java
@@ -0,0 +1,13 @@
+package com.supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierAssistanceFacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierAssistanceFacadeApplication.class, args);
+	}
+
+}
